743a581d433116fb440dc0d51d4d9a40b43121ec
deviego <deviego4@gmail.com>
2023-03-21 10:27:20 -0300

    middleware  add

--- src/server.js
+++ src/server.js
@@ -1,23 +1,30 @@
 import http from 'node:http'
+import json from './middleware/json.js'
 
 
 const users = []
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const {method, url} = req
+ 
+ await json(req, res)
+  
+
   if(method == 'GET'  &&  url == '/users'){
     return res
-    .setHeader('Content-type', 'application/json')
     .end(JSON.stringify(users))
   }
 
   if(method == 'POST' && url == '/users'){
+    const {name,  email} = req.body
     users.push({
       id:1,
-      name: 'Diego',
-      email:'diegodomingues266@gmail.com'
+      name,
+      email,
     })
-    return res.end('Criação bem sucedida')
+
+    return res.writeHead(201).end()
   }
+  return res.writeHead(401).end()
 })
 server.listen(3333)
  
